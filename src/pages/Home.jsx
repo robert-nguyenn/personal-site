5ff1d07589404fedc19aab727c0d756fa4b5abf2
Robert <robert.nguyenanh@gmail.com>
2025-05-22 15:56:44 -0400

    feat: Remove ContactSection and Footer from Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,6 @@ import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
-import { ContactSection } from "../components/ContactSection";
-import { Footer } from "../components/Footer";
 
 export const Home = () => {
   return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -22,9 +20,6 @@ export const Home = () => {
       <AboutSection />
       <SkillsSection />
       <ProjectsSection />
-      <ContactSection />
     </main>
-    {/* Footer */}
-    <Footer />
   </div>;
 }
